bc5130ac5a0be158b716872b346c852d75262ecd
Ezequiel Migueles <ezequielmigueles@icloud.com>
2021-08-31 17:11:49 +0200

    routes created

--- app.js
+++ app.js
@@ -2,12 +2,16 @@ const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
 const app = express();
-const port = process.env.PORT;
 
+const routes_users = require("./routes/users.routes");
+const routes_api = require("./routes/api.routes");
+
+const port = process.env.PORT;
 
 //Middleware
 app.use(cors());
-app.use(express.urlencoded({
+app.use(
+  express.urlencoded({
     extended: false,
   })
 );
@@ -19,6 +23,9 @@ app.set("view engine", "pug");
 app.set("views", "./views");
 
 //Routes
+
+app.use("/", routes_users);
+app.use("/api", routes_api);
 
 app.get("*", (req, res) => {
   res.status(404).render("404");
