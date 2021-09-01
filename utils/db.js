a8b1b11041c5c05c03aa36c409ba90bcd60878c5
Ezequiel Migueles <ezequielmigueles@icloud.com>
2021-09-01 15:29:57 +0200

    db connection stablished

--- utils/db.js
+++ utils/db.js
@@ -1,15 +1,13 @@
 const mongoose = require("mongoose");
 
-mongoose.connect(process.env.MONGODB_URI, {
+mongoose.connect( process.env.MONGODB_URI, {
   useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useCreateIndex: true,
-  useFindAndModify: false,
 });
 
 const db = mongoose.connection;
 
-db.on("error", (error) => console.log(error));
-db.once("open", () => console.log("Connection to db established"), initial());
+db.on("error", error => console.log(error));
+db.once("open", () => console.log("Connection to db established"));
+
 
 module.exports = mongoose;
