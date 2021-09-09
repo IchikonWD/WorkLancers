1219320ba474ac186a7e96d3066de136fd2a5d1d
gugohh <oohhgug@gmail.com>
2021-09-09 23:36:40 +0200

    change loginpug-logincss,registerpug-registercss,added register2pug-register and controllers/routs for register2

--- routes/users.routes.js
+++ routes/users.routes.js
@@ -5,6 +5,7 @@ const pages = require ("../controllers/views") //·Importamos el controlador de 
 // User routes
 router.get("/", pages.home);
 router.get("/register", pages.register);
+router.get("/register2", pages.register2);
 router.get("/favorites", pages.favorites);
 router.get("/login",pages.login);
 router.get("/profile");
