144b603471ede0fe966b78da550420b7e8867cd5
Drag0s98 <dragostanase1698@gmail.com>
2021-09-16 09:53:22 +0200

    scraping changes

--- utils/scraperTwo.js
+++ utils/scraperTwo.js
@@ -10,13 +10,15 @@ const extractJobs = (link, browser) => new Promise(async (resolve, reject) => {
 
         await page.waitForSelector('h1')
 
-        jobData['jobTitle'] = await page.$eval('h2', (title) => title.innerText)
-        
+        jobData['jobTitle'] = await page.$eval('#productName > h1', (title) => title.innerText)
+
         jobData['jobDescription'] = await page.$eval('#app > div > div.container.main > section > section.box-common.block-project > div > section > article:nth-child(1) > div.expander.js-expander-passed', (description) => description.innerText)
 
-         jobData['jobBudget'] = await page.$eval('#app > div > div.container.main > section > section.box-common.block-project > div > section > article:nth-child(1) > div.row > div:nth-child(2) > h4', (payment) => payment.innerText)
+        jobData['jobBudget'] = await page.$eval('#app > div > div.container.main > section > section.box-common.block-project > div > section > article:nth-child(1) > div.row > div:nth-child(2) > h4', (payment) => payment.innerText)
 
         jobData['jobTimer'] = await page.$eval('#productName > p', (timer) => timer.innerText)
+
+        jobData['jobUrl'] = await page.url();
 
         resolve(jobData)
     } catch (err) {
@@ -40,19 +42,19 @@ const scraperTwo = async (url) => {
         for (let i = 1; i < 2; i++) {
 
             await page.goto(`${url}&page=${i}`)
-            
-             const urlsPage = await page.$$eval('#projects > div > div.project-header > h2 > a', (link) => link.map(link => link.href))
+
+            const urlsPage = await page.$$eval('#projects > div > div.project-header > h2 > a', (link) => link.map(link => link.href))
             // console.log('URLS capturadas: ', urlsPage.length, urlsPage);
-             urls.push(...urlsPage)
+            urls.push(...urlsPage)
         }
         // console.log(urls);
-       
-        let newUrl = urls.slice(0,5)
-        for(jobsLink in newUrl){
+
+        let newUrl = urls.slice(0, 5)
+        for (jobsLink in newUrl) {
             const jobs = await extractJobs(newUrl[jobsLink], browser)
             // console.log(urls[jobsLink]);
             scraperData.push(jobs)
-           // console.log(jobs);
+            // console.log(jobs);
         }
         //console.log(scraperData, "Lo que duelve mi funcion scraper", scraperData.length);
         browser.close()
